074f488023591f1bac47898ebda33719273d73a9
20215ganesh <muzinoasahi@gmail.com>
2024-03-04 01:04:56 +0530

    third commit

--- src/components/courseIn.tsx
+++ src/components/courseIn.tsx
@@ -4,51 +4,53 @@ export default function CourseIn(){
         title: string,
         img: string ,
         desc1: string ,
-        desc2: null
     }>({
         title:'Finance & Accounting',
         img:'FA-course.jpg',
         desc1:'The Financial Management course taught me an excellent knowledge of essential factors for successful business decisions. This involves a thorough understanding of bonds, including their valuation techniques, which were crucial in financial planning. Understanding the computation of Net Present Value (NPV) and Future Value (FV) equips businesses with essential tools for evaluating projects and selecting strategic initiatives. The course provided me with knowledge about risk management, allowing for an advanced approach to financial decision-making and a thorough comprehension of Return on Investment (ROI) measures, crucial for assessing project profitability and influencing strategic financial decisions.',
-        desc2:null
     });
     console.log(selected,'sel')
     const courseArray=[
         {
             title:'Finance & Accounting',
             img:'FA-course.jpg',
-            desc1:'The Accounting and Financial Management courses have been crucial to my journey. Exploring the subtleties of accounting principles has equipped me with the skills to interpret and analyze financial statements. Understanding this information is essential for making financial decisions and gaining insight into an organization\'s economic well-being.',
-            desc2:'Delving deeply into financial reporting, budgeting, and risk management has offered valuable insights into finance\'s crucial role in organizational success. Deciphering financial statements has been essential, enabling me to contribute to business operations strategically.'
+            desc1:'The Financial Management course taught me an excellent knowledge of essential factors for successful business decisions. This involves a thorough understanding of bonds, including their valuation techniques, which were crucial in financial planning. Understanding the computation of Net Present Value (NPV) and Future Value (FV) equips businesses with essential tools for evaluating projects and selecting strategic initiatives. The course provided me with knowledge about risk management, allowing for an advanced approach to financial decision-making and a thorough comprehension of Return on Investment (ROI) measures, crucial for assessing project profitability and influencing strategic financial decisions.',
         },{
             title:'Marketing',
             img:'M-course.jpg',
             desc1:'In the Marketing Management course, I learned essential knowledge about market planning, focusing on the importance of thorough planning techniques and their implementation. I gained an in-depth understanding of market analysis techniques, which enabled me to evaluate the market environment successfully. Studying the 4Ps (Product, Price, Place, and Promotion) gives a comprehensive understanding of the marketing mix, highlighting their crucial impact on marketing strategies and decision-making. This course has equipped me with the expertise and resources needed for strategic marketing planning, market analysis, and implementing the 4Ps to make well-informed decisions in ever-changing corporate environments.',
-            desc2:null
+            
         },{
             title:'Ethics & CSR',
             img:'EC-course.jpg',
             desc1:'Ethics & CSR has served as a guiding principle in the business world. It has made me more aware of ethical considerations in business decisions and actions. Recognizing the social responsibility of organizations has enhanced my ethical decision-making skills and emphasized the significance of sustainable and responsible business practices.',
-            desc2:null
+            
         },{
             title:'Business Analytics',
             img:'BA-course.jpg',
             desc1:'The Business Analytics course provided valuable insights, particularly a deep understanding of expected value and its crucial role in decision-making. I have developed proficiency in hypothesis testing, which allows me to make well-informed decisions on various projects. The training improved my skills in multi-regression analysis, allowing me to examine complicated datasets and get valuable insights for strategic decision-making. The main insights have broadened my analytical skills and enabled me to make valuable contributions to data-driven decision-making in various business situations. ',
-            desc2:null
+            
         },{
             title:'Operations Management',
             img:'OP-course.jpg',
             desc1:'Exploring ways to streamline processes and optimize efficiency is crucial in business operations management. From supply chain management to quality control, the course has offered valuable insights into operational excellence. Understanding this is crucial for maintaining smooth business operations and providing value to customers.',
-            desc2:null
+            
         },{
             title:'Digital Transformation',
             img:'DT-course.jpg',
             desc1:'The dynamics of technology have reshaped business models through Digital Transformation. Grasping the fusion of digital technologies, e-commerce, and data-driven decision-making has been truly revolutionary. This course has provided me with valuable knowledge on how to navigate the digital landscape and achieve organizational success in today\'s digital age.',
-            desc2:null
+            
         }
     ]
     const handleButtonClick = (title: string) => {
-        const filtered = courseArray.filter(course => course.title === title);
-        setSelected(filtered);
+        const filtered = courseArray.find(course => course.title === title);
+        setSelected(filtered || {
+          title: 'Finance & Accounting',
+          img: 'FA-course.jpg',
+          desc1: 'The Financial Management course taught me an excellent knowledge of essential factors for successful business decisions. This involves a thorough understanding of bonds, including their valuation techniques, which were crucial in financial planning. Understanding the computation of Net Present Value (NPV) and Future Value (FV) equips businesses with essential tools for evaluating projects and selecting strategic initiatives. The course provided me with knowledge about risk management, allowing for an advanced approach to financial decision-making and a thorough comprehension of Return on Investment (ROI) measures, crucial for assessing project profitability and influencing strategic financial decisions.',
+        });
       };
+      
       
     return(
         <div className="   relative flex flex-col  gap-6 items-center justify-center h-[120vh] md:h-[140vh] w-screen snap-center">
@@ -73,7 +75,7 @@ export default function CourseIn(){
 
                     <div className=' flex flex-col m-2 w-full md:w-[60%] mx-[40px]   overflow-y-scroll scrollbar-none'>
                         <span className=" uppercase  p-1 md:p-2 text-[12px] md:text-[30px] font-bold">{selected.title}</span>
-                        <p className=' m-1 my-5 text-[10px] md:text-[20px] '>{selected.desc1}{selected?.desc2}</p>
+                        <p className=' m-1 my-5 text-[10px] md:text-[20px] '>{selected.desc1}</p>
                         <p className=' m-1 my-5 text-[10px] md:text-[20px]'></p>
                     </div>
             </div>
