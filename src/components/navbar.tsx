0eb17e64f9b8f90140c039fa9f39ee3b4046bc06
Ganesh_web_dev <115787522+GaneshWebDev@users.noreply.github.com>
2024-11-13 11:39:39 +0530

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -27,7 +27,7 @@ export default function Navbar({ hero, about,  project, certificate, skills, con
         // Example ref: file-207fd9951e759130053d37cf0a558ffe84ddd1c9-mp3
         // We don't need the first part, unless we're using the same function for files and images
         const [_file, id, extension] = ref.split('-');
-        return `https://cdn.sanity.io/files/${PROJECT_ID}/${DATASET}/${id}.${extension}`
+        return `https://cdn.sanity.io/files/${process.env.PROJECT_ID}/${process.env.DATASET}/${id}.${extension}`
       }
     const handleMenuClick = (ref: RefObject<HTMLDivElement>) => {
         ref.current?.scrollIntoView({ behavior: 'smooth' });
