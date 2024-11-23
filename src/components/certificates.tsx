11d67dee19b3b0e552d45fd113d35224ea066599
Ganesh_web_dev <115787522+GaneshWebDev@users.noreply.github.com>
2024-11-23 12:09:18 +0530

    Update certificates.tsx

--- src/components/certificates.tsx
+++ src/components/certificates.tsx
@@ -104,7 +104,7 @@ useEffect(()=>{
         viewport={{
           once:true
         }}
-      className=" h-screen relative flex flex-col items-center justify-center overflow-hidden mt-10 md:mt-20 ">
+      className=" h-screen relative flex flex-col items-center justify-center overflow-hidden mt-10 ">
         <h1 className=" absolute  top-20 md:top-24 mt-10 md:mt-0  uppercase tracking-[10px]">Certificates</h1>
        <div ref={certificateRef} className=" flex snap-x snap-mandatory overflow-x-scroll  scroll-smooth w-5/6 h-full scrollbar-none  md:h-3/5 mt-6 md:mt-0 " >
             {data1?.map((certificate:any)=>{
