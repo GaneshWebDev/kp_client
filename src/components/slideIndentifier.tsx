a8ad653e2482accb646eebd2dd763b5b4ed2dd92
Ganesh_web_dev <115787522+GaneshWebDev@users.noreply.github.com>
2024-11-23 12:08:17 +0530

    Update slideIndentifier.tsx

--- src/components/slideIndentifier.tsx
+++ src/components/slideIndentifier.tsx
@@ -8,7 +8,7 @@ export default function SlideIndicator({num,index}:indentifierProps){
     return(
         <div className="absolute flex gap-5 bottom-20 md:bottom-24 mt-10 md:mt-0 text-gray-500 left- uppercase tracking-[12px]">
             {indicaters.map(i=>{
-                return<div className={`h-2 w-2 rounded-full ${index===i?'bg-blue-700':'bg-[white]'}`}></div>
+                return<div className={`h-2 w-2 rounded-full ${index===i?'bg-blue-700':'bg-gray-400'}`}></div>
             })}
         </div>
     )
